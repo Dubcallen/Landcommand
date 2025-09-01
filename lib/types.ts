1058acbb9b2c3163f4fdc0fcb6ac6561ec6ca387
Dubcallen <WESTONC.ALLEN@GMAIL.COM>
2025-09-01 10:12:53 -0700

    Update types.ts

--- lib/types.ts
+++ lib/types.ts
@@ -1,1 +1,10 @@
+// Define the Filters type you were trying to use
+export type Filters = {
+  market?: string;
+  minAcreage?: number;
+  maxAcreage?: number;
+  status?: "Available" | "Under Contract" | "Sold";
+  priceMin?: number;
+  priceMax?: number;
+};
 
